76da00e253bd0132dca994cfe6f76fe50164cffe
saeid721 <saeid721km@gmail.com>
2024-09-08 18:35:29 +0600

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:islamic_songs_lyrics/main.dart';
+import 'package:pinaki/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const IslamicSongsLyricsApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
